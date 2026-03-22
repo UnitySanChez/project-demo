c3d0a566398980b8176ded33acefecc6abce2877
zxm <477@example.com>
2026-03-22 12:25:09 +0800

    第二次，加入日志类型

--- config/config.go
+++ config/config.go
@@ -1,9 +1,33 @@
 // 存放配置信息
 package config
 
-import "fmt"
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"project-demo/utils/logs"
+)
+
+const (
+	TimeFormat = "2006-01-02 15:04:05"
+)
+
+func initLogConfig(logLevel string) {
+	if logLevel == "debug" {
+		logrus.SetLevel(logrus.DebugLevel)
+	} else if logLevel == "info" {
+		logrus.SetLevel(logrus.InfoLevel)
+	}
+	//文件名和行号
+	logrus.SetReportCaller(true)
+	//日志格式json
+	logrus.SetFormatter(&logrus.JSONFormatter{TimestampFormat: TimeFormat})
+}
 
 func init() {
 	// 初始化配置
-	fmt.Println("初始化配置,测试显示一下")
+	logs.Debug(nil, "开始加载程序配置")
+	viper.SetDefault("LOG_LEVEL", "info")
+	viper.AutomaticEnv()
+	logLevel := viper.GetString("LOG_LEVEL") //获取程序配置
+	initLogConfig(logLevel)
 }
--- main.go
+++ main.go
@@ -5,11 +5,14 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	_ "project-demo/config"
+	"project-demo/utils/logs"
 )
 
 func main() {
 	//1. 初始化配置
 	//2.配置gin
 	r := gin.Default()
+	logs.Info(nil, "项目启动成功")
 	r.Run()
+
 }
